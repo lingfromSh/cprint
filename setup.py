44c4aa2e3a42d8ed60e660e53adf12d009f1d332
Stephen Ling <lingfromsh@163.com>
2020-08-11 00:34:49 +0800

    Update setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='cprint',
+    version='0.1',
+    description='A simple tool for printing colorful text.',
+    author='Stephen Ling',
+    author_email='lingfromsh@163.com',
+    url='https://github.com/lingfromSh/cprint'
+)
